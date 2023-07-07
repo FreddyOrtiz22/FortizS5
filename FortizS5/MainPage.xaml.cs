4c0fb340d980647813b0b814403314e5a4f90ac8
Freddy Ortiz <e1715078984@uisrael.edu.ec>
2023-07-07 17:11:06 -0500

    Agregar archivos de proyecto.

---
+++ FortizS5/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FortizS5
+{
+    public partial class MainPage : ContentPage
+    {
+        private string Url = "http://192.168.100.116/ws_uisrael/post.php";
+        private HttpClient cliente = new HttpClient();
+        private ObservableCollection<estudiante> post;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            ObtenerDatos();
+        }
+        private async void ObtenerDatos()
+        {
+            var contenido = await cliente.GetStringAsync(Url);
+            List<estudiante> listaPost = JsonConvert.DeserializeObject<List<estudiante>>(contenido);
+            post = new ObservableCollection<estudiante>(listaPost);
+            listaestudiante.ItemsSource = post;
+        }
+
+
+        private async void btnMostrar_Clicked(object sender, EventArgs e)
+        {
+            var contenido = await cliente.GetStringAsync(Url);
+            List<estudiante> listaPost = JsonConvert.DeserializeObject<List<estudiante>>(contenido);
+            post = new ObservableCollection<estudiante>(listaPost);
+            listaestudiante.ItemsSource = post;
+
+        }
+    }
+}
---
+++ FortizS5/estudiante.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FortizS5
+{
+    public class estudiante
+    {
+        public int Codigo { get; set;} 
+        public string Nombre { get; set;}
+        public string Apellido { get; set;}
+        public int Edad { get; set;}
+    }
+}
+
